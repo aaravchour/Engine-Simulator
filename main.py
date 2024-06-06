430213aaf8a0f358175f248a37fe09181184cdd7
aaravchour <a.chourishi@outlook.com>
2024-06-06 20:50:36 +0100

    added battery logic

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import playsound
-from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget, QPushButton, QSlider, QProgressBar
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QVBoxLayout,
+    QWidget,
+    QPushButton,
+    QSlider,
+    QProgressBar,
+)
 from PyQt5.QtCore import QTimer, Qt, QUrl
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 
 import sys
-sys.path.insert(0, 'Systems')
 
-from Systems.ui import ui
+sys.path.insert(0, "Systems")
 
-
-
+from Systems.ui import EngineSimulator
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication([])
-    ex = ui()
+    ex = EngineSimulator()
     app.exec_()
